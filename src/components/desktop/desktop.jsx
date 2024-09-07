2c04f9fcda84b8fa95c5127eb354daa2a72b0c75
hahhen <arthurmp2007@hotmail.com>
2024-09-06 23:55:47 -0300

    Folder images based on supabase

--- src/components/desktop/desktop.jsx
+++ src/components/desktop/desktop.jsx
@@ -11,7 +11,7 @@ export default async function Desktop() {
 
     let { data: album, error1 } = await supabase
         .from('album')
-        .select('id, title, cover, slug, video')
+        .select('id, title, cover, slug, video, folder_icon')
     let { data: song, error2 } = await supabase
         .from('song')
         .select('id, title, albumid, url, track_number')
@@ -20,7 +20,7 @@ export default async function Desktop() {
         <div className="w-full p-2 flex justify-end">
             <div className="grid grid-rows-3 z-10 grid-flow-col gap-4">
                 {album.map((album) => (
-                    <Folder key={album.id} video={album.video} slug={album.slug} icon="/folder.png" cover={album.cover} items={song.filter(song => song.albumid == album.id)} title={album.title} />
+                    <Folder key={album.id} video={album.video} slug={album.slug} icon={album.folder_icon ? album.folder_icon:"/folder.png"} cover={album.cover} items={song.filter(song => song.albumid == album.id)} title={album.title} />
                 ))}
                 <Link className='flex flex-col gap-1 items-center' target='_blank' href={"https://docs.google.com/spreadsheets/d/1wFj-7910YrbfznDPqTQ7qJqVrRRBUGgSlayCSaibw4Q/edit?gid=0#gid=0"}>
                     <Image src={"/text.png"} alt='App' width={40} height={40} />
