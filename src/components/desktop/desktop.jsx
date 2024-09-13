72d663c03c2e36f8c602e7da0d9ca70f51126f5f
hahhen <arthurmp2007@hotmail.com>
2024-09-13 08:14:43 -0300

    Moved folders to the right

--- src/components/desktop/desktop.jsx
+++ src/components/desktop/desktop.jsx
@@ -19,15 +19,15 @@ export default async function Desktop() {
     return (
         <div className="w-full p-2 flex justify-end">
             <div className="grid grid-rows-3 z-10 grid-flow-col gap-4">
-                {album.map((album) => (
-                    <Folder key={album.id} video={album.video} slug={album.slug} icon={album.folder_icon ? album.folder_icon:"/folder.png"} cover={album.cover} items={song.filter(song => song.albumid == album.id)} title={album.title} />
-                ))}
                 <Link className='flex flex-col gap-1 items-center' target='_blank' href={"https://docs.google.com/spreadsheets/d/1wFj-7910YrbfznDPqTQ7qJqVrRRBUGgSlayCSaibw4Q/edit?gid=0#gid=0"}>
                     <Image src={"/text.png"} alt='App' width={40} height={40} />
                     <span className="bg-headerbg">
                         TLOG TRACKER
                     </span>
                 </Link>
+                {album.map((album) => (
+                    <Folder key={album.id} video={album.video} slug={album.slug} icon={album.folder_icon ? album.folder_icon : "/folder.png"} cover={album.cover} items={song.filter(song => song.albumid == album.id)} title={album.title} />
+                ))}
             </div>
         </div>
     )
